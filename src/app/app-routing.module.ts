fab4ec5dc83514b54edd7bc7172af115169b97c5
Raj Jadhav <raj.jadhav@mrisoftware.com>
2023-03-15 15:02:44 +0530

    Configured routes of the application for different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { JoinRoomPageComponent } from './join-room-page/join-room-page.component';
+import { NewRoomPageComponent } from './new-room-page/new-room-page.component';
+import { RoomPageComponent } from './room-page/room-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'new-room', component: NewRoomPageComponent },
+  { path: 'join-room', component: JoinRoomPageComponent },
+  { path: ':room-id', component: RoomPageComponent },
+  { path: '**', redirectTo: '/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
