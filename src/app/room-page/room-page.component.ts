d6bccb94331f1321df01ec0da587a97d38b645aa
Raj Jadhav <raj.jadhav@mrisoftware.com>
2023-03-18 20:22:28 +0530

    Created room and user models, room-data service and socket-io service

--- src/app/room-page/room-page.component.ts
+++ src/app/room-page/room-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketIoService } from '../Services/socket-io.service';
 
 @Component({
   selector: 'app-room-page',
   templateUrl: './room-page.component.html',
   styleUrls: ['./room-page.component.css']
 })
-export class RoomPageComponent {
+export class RoomPageComponent implements OnInit {
+  private socketIoService: SocketIoService;
 
+  constructor(socketIoService: SocketIoService) {
+    this.socketIoService = socketIoService;
+  }
+
+  ngOnInit(): void {
+
+  }
 }
